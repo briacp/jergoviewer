0ef26dde4baeaff7a667811224132af01217b89e
briacp <briacp@gmail.com>
2014-02-10 10:25:05 +0100

    Sparklines  + Altitude
    Added 4 sparklines graphs (pulse, speed, altitude, power). The bike
    power should increase/decrease according to the altitude variation.

--- briac.net/www/js/run.js
+++ briac.net/www/js/run.js
@@ -2,24 +2,18 @@
 /*global google:false, sprintf:false */
 $(function () {
     "use strict";
-    var //heartBeat = 36,
-    delay = 500,
-        currentStepIndex = 0,
-        lastDistance = 0,
-        runMap,
-        marker,
-        runTimer,
-        heartState = true;
+    var delay = 500,
+        runMap, marker, runTimer, beats = [], speeds = [], alts = [], powers = [];
 
-    $("#stopRun").click(function () {
-        window.clearInterval(runTimer);
+    $("#share").share({
+        url: "http://briac.net/jergoviewer",
+        text: "StreetView in you living room while you exercise"
     });
-    $("#showInfo").click(function () {
-        $("#info").toggle();
-    });
-    $("#showMap").click(function () {
-        $("#map").toggle();
-    });
+
+    $("#stopRun").click(function ()    { window.clearInterval(runTimer); });
+    $("#showSparks").click(function () { $("#sparks").toggle(); });
+    $("#showInfo").click(function ()   { $("#info").toggle(); });
+    $("#showMap").click(function ()    { $("#map").toggle(); });
 
     $.getJSON("run.json", function (data) {
         initRun(data);
@@ -34,15 +28,29 @@ $(function () {
             finishRun();
         }
 
-        var  currentLatLng = new google.maps.LatLng(d.point.lat, d.point.lon);
+        // Refresh the sparklines ==============================================
+        speeds.push(d.speed / 10);
+        beats.push(d.pulse === 0 ? null : d.pulse);
+        alts.push(Math.floor(d.point.alt));
+        powers.push(d.destPower);
+
+        $("#sparkSpeed").sparkline(speeds,   { width: "100%", defaultPixelsPerValue: 1, fillColor: "rgba(68, 68, 238, .5)", tooltipPrefix: "Speed: ",    tooltipSuffix: "km/h"  });
+        $("#sparkHeart").sparkline(beats,    { width: "100%", defaultPixelsPerValue: 1, fillColor: "rgba(238, 68, 68, .5)", tooltipPrefix: "Pulse: ",    tooltipSuffix: "bpm" });
+        $("#sparkAltitude").sparkline(alts,  { width: "100%", defaultPixelsPerValue: 1, fillColor: "rgba(68, 238, 68, .5)", tooltipPrefix: "Altitude: ", tooltipSuffix: "m" });
+        $("#sparkPower").sparkline(powers,   { width: "100%", defaultPixelsPerValue: 1, fillColor: "rgba(255, 22, 22, .5)", tooltipPrefix: "Power: ",    tooltipSuffix: "W" });
+
+        // Refresh the map  ====================================================
+        var currentLatLng = new google.maps.LatLng(d.point.lat, d.point.lon);
         marker.setPosition(currentLatLng);
         runMap.panTo(currentLatLng);
 
+        // Refresh the interface ===============================================
         $("#street").attr("src", d.img);
         $("#heartbeat").html(d.pulse);
-        $("#distance").html(sprintf("%.2f", d.calcDistance / 1000) + "&nbsp;km");
-        $("#time").html(d.time);
+        $("#distance").html(sprintf("%.2f", d.calcDistance / 1000) + "km");
+        $("#time").html(d.time + "<br/><hr/>" + ( isFinite(d.pace) ? d.pace + "mn/km" : "--" ));
 
+        // Refresh the info ====================================================
         $("#info").html(
             "<dl>" +
             "<dl>pedalRpm</dl><dd>" + d.pedalRpm + "</dd>" +
@@ -50,11 +58,12 @@ $(function () {
             "<dl>speed</dl><dd>" + d.speed + "</dd>" +
             "<dl>destPower</dl><dd>" + d.destPower + "</dd>" +
             "<dl>energy</dl><dd>" + d.energy + "</dd>" +
-            "<dl>p.lat</dl><dd>" + sprintf("%.4f",d.point.lat) + "</dd>" +
-            "<dl>p.lon</dl><dd>" + sprintf("%.4f",d.point.lon) + "</dd>" +
-            "<dl>p.alt</dl><dd>" + sprintf("%.2f",d.point.alt) + "</dd>" +
+            "<dl>p.lat</dl><dd>" + sprintf("%.4f", d.point.lat) + "</dd>" +
+            "<dl>p.lon</dl><dd>" + sprintf("%.4f", d.point.lon) + "</dd>" +
+            "<dl>p.alt</dl><dd>" + sprintf("%.2f", d.point.alt) + "</dd>" +
             "<dl>p.dist</dl><dd>" + d.point.distance + "</dd>" +
             "<dl>calcDistance</dl><dd>" + sprintf("%.2f", d.calcDistance) + "</dd>" +
+            "<dl>deltaAltitude</dl><dd>" + d.deltaAltitude + "</dd>" +
             "</dl>");
     }
 
@@ -65,20 +74,18 @@ $(function () {
         var currentLatLng = new google.maps.LatLng(data.point.lat, data.point.lon);
         runMap = new google.maps.Map(document.getElementById("map"), {
             center: currentLatLng,
-             mapTypeControl: false,
+            mapTypeControl: false,
             streetViewControl: false,
             scaleControl: false,
-            panControl:false,
-            overviewMapControl:false,
+            panControl: false,
+            overviewMapControl: false,
             zoomControl: false,
             zoom: 14
         });
         marker = new google.maps.Marker({
             position: currentLatLng,
             map: runMap
-            //title: "Hello World!"
         });
-        //google.maps.event.addDomListener(window, 'load', initialize);
 
 
         // Kinetic stuff ====================================
@@ -107,8 +114,6 @@ $(function () {
 
         // Timer stuff ===================================
         runTimer = window.setInterval(function () {
-            heartState = heartState ? false : true;
-
             $.getJSON("run.json", function (d) {
                 updateRun(d);
             });
@@ -116,5 +121,4 @@ $(function () {
     }
 
 
-});
-
+});
